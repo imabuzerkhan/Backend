5ad36a91a22e47721b03b10e867a8abbaaaae3b8
imabuzerkhan <abuzerk41@gmail.com>
2024-07-29 15:04:40 +0545

    Indexof

---
+++ Day1/indexof.js
@@ -0,0 +1,13 @@
+//  example of indexof
+
+// let text = "Hello world, welcome to the universe.";
+// let resu = text.indexOf('to')
+// console.log(resu)
+
+// let text = "Hello world, welcome to the universe.";
+// let resu = text.indexOf('e')
+// console.log(resu)
+
+let text = "Hello world, welcome to the universe.";
+let resu = text.indexOf('e', 7 )
+console.log(resu)
