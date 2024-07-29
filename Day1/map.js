8fcce4e70660ee7549676b995592bc859988de9c
imabuzerkhan <abuzerk41@gmail.com>
2024-07-29 14:46:18 +0545

    without map

---
+++ Day1/map.js
@@ -0,0 +1,22 @@
+// map method in js
+
+// ** syntax of map method
+
+//? new araay = newarray.map((currentvlaue , [index , [array]])=>{
+
+// ?},thisArg )
+
+// ** You have an array of numbers and you want to double each number in the array.
+
+// ** without map
+
+let number = [2, 4, 5, 6];
+let doublenumber = [];
+
+for (let i = 0; i < number.length; i++) {
+  doublenumber.push(number[i] * 2);
+}
+
+console.log(doublenumber);
+
+// ** with map
