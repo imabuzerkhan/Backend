9019ba245e2e9db7417ec4155c8cdb02c9e4b566
imabuzerkhan <abuzerk41@gmail.com>
2024-07-31 18:08:31 +0545

    append in node js

--- Day2/script.js
+++ Day2/script.js
@@ -1,9 +1,19 @@
 const fs = require('fs')
-// how to create a file in node js
-fs.writeFile("hey.txt" , "hello iam abuzer khan", function(err){
-if(err){
-  console.err("error")
-}else{
-  console.log("done")
-}
-} )
+// ** how to create a file in node js
+// fs.writeFile("hey.txt" , "hello iam abuzer khan", function(err){
+// if(err){
+//   console.err("error")
+// }else{
+//   console.log("done")
+// }
+// } )
+
+// ** append file
+fs.appendFile("hey txt" , "kaise hai bhai log", function(err){
+  if(err){
+    console.log("err")
+
+  }else{
+    console.log("done")
+  }
+})
